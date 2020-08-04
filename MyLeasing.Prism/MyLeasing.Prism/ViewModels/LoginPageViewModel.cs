37677d7593066ad51533bee77a73a68fb7a48148
LUIS CARLOS SANCHEZ CABRERA <lc.sanchez@udla.edu.co>
2020-08-03 19:36:00 -0500

    check internet Connection

--- MyLeasing.Prism/MyLeasing.Prism/ViewModels/LoginPageViewModel.cs
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/LoginPageViewModel.cs
@@ -57,12 +57,22 @@ namespace MyLeasing.Prism.ViewModels
             }
             IsRunning = true;
             IsEnable = false;
+            var url = App.Current.Resources["UrlAPI"].ToString();
+            var connection = await _apiService.CheckConnectionAsync(url);
+            if (!connection)
+            {
+                IsEnable = true;
+                IsRunning = false;
+                await App.Current.MainPage.DisplayAlert("Error", "Check the internet connection.", "Accept");
+                return;
+            }
+
             var request = new TokenRequest
             {
                 Password = Password,
                 Username = Email
             };
-            var url = App.Current.Resources["UrlAPI"].ToString();
+            
             var response = await _apiService.GetTokenAsync(url, "Account", "/CreateToken", request);
             IsEnable = true;
             IsRunning = false;
